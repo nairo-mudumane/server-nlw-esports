a67538bda7be76d4fcce01b5d3e1421e2677983b
nairo-mudumane <nairomudumane@outlook.com>
2022-11-14 15:19:36 +0200

    cors configuration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(3333, () => console.log("running"));
